2c5c877d0c7dc3946368e4c4ded6f59164fd70da
TANIGUCHI Takaki <takaki@asis.media-as.org>
2017-04-17 23:05:27 +0900

    typescript migration

---
+++ src/App.tsx
@@ -0,0 +1,136 @@
+import * as React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
+import './App.css';
+import {Divider, DropDownMenu, List, ListItem, MenuItem, RaisedButton} from 'material-ui';
+import {AvPause, AvPlayArrow, ImageTimer, ImageTimerOff} from 'material-ui/svg-icons';
+import {createStore} from 'redux';
+import {createAction, handleActions} from 'redux-actions';
+import {connect, Dispatch, Provider} from 'react-redux';
+import DataStore, {TimerEntry} from './model';
+import Component = React.Component;
+
+injectTapEventPlugin();
+
+interface GameTimerProps {
+    store: DataStore;
+    updateStore: (d: DataStore) => void;
+}
+
+class GameTimer extends Component<GameTimerProps, undefined> {
+
+    static timeformat_(d: number) {
+        const m = Math.floor(d / 60);
+        const s = d % 60;
+        return `${m}:${(s < 10 ? '0' + s : s)}`;
+    }
+
+    componentWillMount() {
+        this.onChange(this.props.store.menuIndex);
+    }
+
+    onChange(menuIndex: number) {
+        const onTick = () => {
+            this.props.updateStore(this.props.store.setTime(this.props.store.sw.toString()));
+        };
+        const onFinish = () => {
+            if (this.props.store.isTimerLeft()) {
+                const store = this.props.store.nextTimer().setSw(onTick, onFinish);
+                this.props.updateStore(store);
+                store.sw.go();
+            } else {
+                const synthes = new SpeechSynthesisUtterance('終了です。');
+                synthes.lang = 'ja-JP';
+                speechSynthesis.speak(synthes);
+                this.props.updateStore(this.props.store.setFinish(true));
+            }
+        };
+        this.props.store.sw.pause();
+        this.props.store.noSleep.disable();
+        this.props.updateStore(this.props.store.setMenuIndex(menuIndex).setSw(onTick, onFinish));
+    }
+
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div>
+                    <DropDownMenu value={this.props.store.menuIndex}
+                                  onChange={(event, index, value) => this.onChange(value)}>
+                        {this.props.store.getNames().toKeyedSeq().map((n, i) =>
+                            <MenuItem value={i} key={i} primaryText={n}/>).toArray()}
+                    </DropDownMenu>
+                    <List>
+                        {this.props.store.getTimerList().toKeyedSeq().map((e: TimerEntry, i) => (
+                            <ListItem
+                                disabled={true}
+                                className="timer-list"
+                                primaryText={`${e.get('title')} (${GameTimer.timeformat_(e.get('duration'))})`}
+                                key={i}
+                                data-is-current={i === this.props.store.timerIndex}
+                                rightIcon={i === this.props.store.timerIndex ? <ImageTimer/> :
+                                    <ImageTimerOff/>}/>
+                        )).toArray()}
+                        <Divider/>
+                        <div className="time-display" data-is-finish={this.props.store.finish}>
+                            <code>{this.props.store.getTime()} </code></div>
+                        <div className="control-buttons">
+                            {this.props.store.finish ? '' : (
+                                <RaisedButton label={this.props.store.label}
+                                              className="button"
+                                              icon={this.props.store.running ? <AvPause/> :
+                                                  <AvPlayArrow/> }
+                                              onClick={() => {
+                                                  this.props.store.noSleep.enable();
+                                                  this.props.updateStore(this.props.store.toggleSwitch());
+                                              }}/>
+                            )}
+                            <RaisedButton className="button" label="Reset" secondary={true} onClick={() => {
+                                this.onChange(this.props.store.menuIndex);
+                            }}/>
+                        </div>
+                    </List>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+}
+
+const initialState = new DataStore();
+
+const updateStore = createAction('UPDATE_MODEL');
+
+const reducer = handleActions<DataStore>(
+    {
+        ['UPDATE_MODEL']: (state, action) => action.payload,
+    },
+    initialState
+);
+
+const store = createStore(reducer);
+
+function mapStateToProps(state: DataStore) {
+    return {store: state};
+}
+
+function mapDispatchToProps(dispatch: Dispatch<DataStore>) {
+    return {
+        updateStore: function (m: DataStore) {
+            dispatch(updateStore(m));
+        }
+    };
+}
+
+interface AppProps {
+}
+
+class App extends Component<AppProps, undefined> {
+    render() {
+        const DApp = connect(mapStateToProps, mapDispatchToProps)(GameTimer);
+        return (
+            <Provider store={store}>
+                <DApp />
+            </Provider>);
+    }
+}
+
+export default App;
---
+++ src/model.ts
@@ -0,0 +1,266 @@
+import {List, Record, Range} from 'immutable';
+import {Option, None} from 'monapt';
+import NoSleep from './nosleep';
+
+export enum SWState {
+    BEFORE_START, RUNNING, SUSPEND, FINISHED
+}
+
+export class StopWatch {
+    title: string;
+    mseconds: number;
+    timeoutIds: Array<number>;
+    checkpoint: Array<number>;
+    onTick: () => void;
+    onFinish: () => void;
+    swstate: SWState;
+    started: Option<Date>;
+
+    constructor(title: string, seconds: number,
+                onTick: () => void = () => {
+                    return;
+                },
+                onFinish: () => void = () => {
+                    return;
+                }) {
+        this.title = title;
+        this.onTick = onTick;
+        this.onFinish = onFinish;
+        this.mseconds = seconds * 1000;
+        this.timeoutIds = [];
+        this.started = None;
+        this.checkpoint = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1].map(i => i * 60).concat(
+            [50, 40, 30, 20, 10, 5, 4, 3, 2, 1]).filter(element => element < seconds);
+        // this.checkpoint = [3 * 60, 2 * 60, 60, 30, 20, 10, 1].filter(element => element < seconds);
+        this.swstate = SWState.BEFORE_START;
+    }
+
+    toString(): string {
+        const totalSeconds = Math.ceil(this.left_() / 1000);
+        if (totalSeconds <= 0) {
+            return `00:00:00`;
+        }
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+        const seconds = Math.floor(totalSeconds % 60);
+
+        return `${(hours < 10 ? '0' + hours : hours )}:${
+            (minutes < 10 ? '0' + minutes : minutes)}:${
+            (seconds < 10 ? '0' + seconds : seconds)}`;
+    }
+
+    toLeftString_(): string {
+        const totalSeconds = Math.ceil(this.left_() / 1000);
+        if (totalSeconds < 10) {
+            return totalSeconds.toString();
+        } else {
+            const hours = Math.floor(totalSeconds / 3600);
+            const minutes = Math.floor((totalSeconds % 3600) / 60);
+            const seconds = Math.floor(totalSeconds % 60);
+            return `残り${(hours > 0 ? hours + '時間' : '')}${
+                (minutes > 0 ? minutes + '分' : '')}${
+                (seconds > 0 ? seconds + '秒' : '')}です`;
+        }
+    }
+
+    go() {
+        if (this.swstate === SWState.RUNNING || this.swstate === SWState.FINISHED) {
+            return;
+        }
+        if (this.swstate === SWState.BEFORE_START) {
+            const synthes = new SpeechSynthesisUtterance(`${this.title}です`);
+            synthes.lang = 'ja-JP';
+            speechSynthesis.speak(synthes);
+        }
+        this.started = Option(new Date());
+        this.timeoutIds.push(setTimeout(() => this.tick_(), 100));
+        this.swstate = SWState.RUNNING;
+    }
+
+    pause() {
+        if (this.swstate === SWState.SUSPEND || this.swstate === SWState.FINISHED) {
+            return;
+        }
+        this.mseconds = this.left_();
+        this.started = None;
+        for (let id of this.timeoutIds) {
+            clearTimeout(id);
+        }
+        this.swstate = SWState.SUSPEND;
+    }
+
+    left_(): number {
+        return this.started.map((started: Date) =>
+            (this.mseconds - ((new Date()).getTime() - started.getTime()))).getOrElse(() => this.mseconds);
+    }
+
+    tick_() {
+        if (this.left_() / 1000 < this.checkpoint[0]) {
+            const synthes = new SpeechSynthesisUtterance(this.toLeftString_());
+            synthes.lang = 'ja-JP';
+            synthes.rate = 1.2;
+            speechSynthesis.speak(synthes);
+            this.checkpoint.shift();
+        }
+        this.timeoutIds.push(setTimeout(
+            () => {
+                this.onTick();
+                if (this.left_() <= 0) {
+                    this.onFinish();
+                    this.swstate = SWState.FINISHED;
+                } else {
+                    this.tick_();
+                }
+            },
+            90));
+    }
+
+    isRun() {
+        return this.swstate === SWState.BEFORE_START || this.swstate === SWState.SUSPEND;
+    }
+}
+
+export class TimerEntry extends Record({
+    title: '',
+    duration: 0
+}) {
+    title: string;
+    durataion: number;
+}
+class MenuEntry extends Record({
+    name: '',
+    timers: List()
+}) {
+    name: string;
+    timers: List<TimerEntry>;
+}
+
+class DataStore extends Record({
+    menuIndex: 0,
+    timerIndex: 0,
+    time: '',
+    label: 'Go',
+    running: false,
+    finish: false,
+    noSleep: new NoSleep(),
+    sw: new StopWatch('dummy', 0),
+}) {
+    static timerMenu = List.of(
+        new MenuEntry({
+            name: 'ディプロマシー',
+            timers: List.of(
+                new TimerEntry({title: '外交フェイズ', duration: 15 * 60}),
+                new TimerEntry({title: '命令記述フェイズ', duration: 5 * 60}),
+                new TimerEntry({title: '命令解決フェイズ', duration: 10 * 60})
+            )
+        }),
+        new MenuEntry({
+            name: 'テスト',
+            timers: List.of(
+                new TimerEntry({title: 'A', duration: 3}),
+                new TimerEntry({title: 'B', duration: 3}),
+                new TimerEntry({title: 'C', duration: 3})
+            )
+        }),
+        new MenuEntry({
+            name: 'テスト2',
+            timers: List.of(
+                new TimerEntry({title: 'A', duration: 1}))
+        })).concat(
+        Range(1, 16).map((i: number) => new MenuEntry({
+            name: `${i}分`,
+            timers: List.of(new TimerEntry({
+                title: `${i}分`, duration: i * 60
+            }))
+        })));
+
+    menuIndex: number;
+    timerIndex: number;
+    time: string;
+    label: string;
+    running: boolean;
+    finish: boolean;
+    noSleep: NoSleep;
+    sw: StopWatch;
+
+    setMenuIndex(i: number) {
+        return new DataStore(this.set('menuIndex', i)).setTimerIndex(0).setFinish(false).setRunning(false);
+    }
+
+    setTimerIndex(i: number) {
+        return new DataStore(this.set('timerIndex', i));
+    }
+
+    setTime(s: string) {
+        return new DataStore(this.set('time', s));
+    }
+
+    setLabel(s: string) {
+        return new DataStore(this.set('label', s));
+    }
+
+    setRunning(b: boolean) {
+        return new DataStore(this.set('running', b));
+    }
+
+    setFinish(b: boolean) {
+        return new DataStore(this.set('finish', b));
+    }
+
+    setSw(onTick: () => void, onFinish: () => void) {
+        return new DataStore(this.set('sw', new StopWatch(this.getTitle(), this.getDuration(), onTick, onFinish)));
+    }
+
+    getCurrentMenu() {
+        return DataStore.timerMenu.get(this.menuIndex);
+    }
+
+    getName() {
+        return DataStore.timerMenu.get(this.menuIndex).get('name');
+    }
+
+    getTimerList(): List<TimerEntry> {
+        return DataStore.timerMenu.get(this.menuIndex).get('timers');
+    }
+
+    getTimer() {
+        return DataStore.timerMenu.get(this.menuIndex).get('timers').get(this.timerIndex);
+    }
+
+    getTitle() {
+        return this.getTimer().get('title');
+    }
+
+    getDuration() {
+        return this.getTimer().get('duration');
+    }
+
+    getTime() {
+        return this.sw.toString();
+    }
+
+    getNames() {
+        return DataStore.timerMenu.map((e: MenuEntry) => e.name);
+    }
+
+    isTimerLeft() {
+        return this.timerIndex + 1 < this.getTimerList().size;
+    }
+
+    nextTimer() {
+        return this.setTimerIndex(this.timerIndex + 1);
+    }
+
+    toggleSwitch() {
+        if (this.sw.isRun()) {
+            this.sw.go();
+            return this.setLabel('Pause').setRunning(true);
+        } else {
+            this.sw.pause();
+            return this.setLabel('Go').setRunning(false);
+        }
+
+    }
+}
+
+export default  DataStore;
