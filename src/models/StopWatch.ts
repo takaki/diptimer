a37635519e3a37d712b7676d284608777fcf9485
TANIGUCHI Takaki <takaki@asis.media-as.org>
2019-02-23 17:18:09 +0900

    union type

--- src/models/StopWatch.ts
+++ src/models/StopWatch.ts
@@ -1,15 +1,14 @@
-import { None, Option } from "monapt";
 import { RemainTime } from "./RemainTime";
 
-enum SWState {
+enum WatchState {
     BEFORE_START, RUNNING, SUSPEND, FINISHED,
 }
 
 export class StopWatch {
     public remainTime1: RemainTime;
     public timeoutIds: number[];
-    public swstate: SWState;
-    public started: Option<Date>;
+    public state: WatchState;
+    public startedAt?: Date;
 
     constructor(public title: string, seconds: number,
                 public onTick: (sw: StopWatch) => void = () => {
@@ -20,37 +19,36 @@ export class StopWatch {
                 }) {
         this.remainTime1 = new RemainTime(seconds * 1000);
         this.timeoutIds = [];
-        this.started = None;
-        this.swstate = SWState.BEFORE_START;
+        this.state = WatchState.BEFORE_START;
+        this.startedAt = undefined;
     }
 
     public go() {
-        if (this.swstate === SWState.RUNNING || this.swstate === SWState.FINISHED) {
+        if (this.state === WatchState.RUNNING || this.state === WatchState.FINISHED) {
             return;
         }
-        if (this.swstate === SWState.BEFORE_START) {
+        if (this.state === WatchState.BEFORE_START) {
             const synthes = new SpeechSynthesisUtterance(`${this.title}です`);
             synthes.lang = "ja-JP";
             speechSynthesis.speak(synthes);
         }
-        this.started = Option(new Date());
+        this.startedAt = new Date();
         this.timeoutIds.push(window.setTimeout(() => this.tick_(), 100));
-        this.swstate = SWState.RUNNING;
+        this.state = WatchState.RUNNING;
     }
 
     public pause() {
-        if (this.swstate === SWState.SUSPEND || this.swstate === SWState.FINISHED) {
+        if (this.state === WatchState.SUSPEND || this.state === WatchState.FINISHED) {
             return;
         }
         this.remainTime1 = this.remainTime();
-        this.started = None;
+        this.startedAt = undefined;
         this.timeoutIds.forEach((id) => clearTimeout(id));
-        this.swstate = SWState.SUSPEND;
+        this.state = WatchState.SUSPEND;
     }
 
     public remainTime(): RemainTime {
-        return this.remainTime1.calc(this.started.map((started: Date) => (new Date()).getTime() - started.getTime())
-            .getOrElse(() => 0));
+        return this.remainTime1.calc(this.startedAt ? Date.now() - this.startedAt.getTime() : 0);
     }
 
     public tick_() {
@@ -58,7 +56,7 @@ export class StopWatch {
             () => {
                 this.onTick(this);
                 if (this.remainTime().finished) {
-                    this.swstate = SWState.FINISHED;
+                    this.state = WatchState.FINISHED;
                     this.onFinish(this);
                 } else {
                     this.tick_();
@@ -68,7 +66,6 @@ export class StopWatch {
     }
 
     public canRun() {
-        return this.swstate === SWState.BEFORE_START || this.swstate === SWState.SUSPEND;
+        return this.state === WatchState.BEFORE_START || this.state === WatchState.SUSPEND;
     }
 }
-
