e5abd0540434adda39bee2aaf56a650281738622
TANIGUCHI Takaki <takaki@asis.media-as.org>
2019-06-11 17:12:53 +0900

    Updates

---
+++ precache-manifest.0a4dc3d101a1e8f1e2aa2db031cee02d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0991cd3555687f5eafb823a479db88b4",
+    "url": "/diptimer/index.html"
+  },
+  {
+    "revision": "512f72d23db85e6ff525",
+    "url": "/diptimer/static/css/main.7105edaa.chunk.css"
+  },
+  {
+    "revision": "ea0f3d081ff563c44e4a",
+    "url": "/diptimer/static/js/2.001aaa96.chunk.js"
+  },
+  {
+    "revision": "512f72d23db85e6ff525",
+    "url": "/diptimer/static/js/main.da29c365.chunk.js"
+  },
+  {
+    "revision": "dc1dedbc71a896ec8970",
+    "url": "/diptimer/static/js/runtime~main.0c6a9737.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/diptimer/precache-manifest.13b752d8b3ea0874e89fcf2e99d47793.js"
+  "/diptimer/precache-manifest.0a4dc3d101a1e8f1e2aa2db031cee02d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/diptimer/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/diptimer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
